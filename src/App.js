5054d08b71f4ec9ef1b9e84b5f3c74e744afbeb3
Strafe <stefankolmogorov28@gmail.com>
2024-09-14 22:20:12 +0100

    test-build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css"
 import { useState } from "react";
 import icon from "./icon-calculator.svg"
+import illustration from "./illustration-empty.svg"
+import { motion, useAnimation } from "framer-motion";
 
 function UserInputField({format, label, formatLeft, value, change, errors}) {
   const [isFocused, setIsFocused] = useState(false);
@@ -41,6 +43,30 @@ export default function Board() {
   const [mortgageTerm, setMortgageTerm] = useState("");
   const [interestRate, setInterestRate] = useState("");
   const [selectedType, setSelectedType] = useState("");
+  const [monthly, setMonthly] = useState(0);
+  const [total, setTotal] = useState(0);
+  const controls = useAnimation();
+  const result = useAnimation();
+
+  const calculateMortgage = (amount, years, interestRate, type) => {
+    amount = parseFloat(amount.replace(/,/g, ''));
+    const P = parseFloat(amount);
+    interestRate = parseFloat(interestRate);
+    const annualInterestRateDecimal = parseFloat(interestRate) / 100;
+    const r = annualInterestRateDecimal / 12; // Monthly interest rate
+    const n = parseFloat(years) * 12; // Total number of payments
+
+
+    if (type === "repayment") {
+      const M = P * r * Math.pow(1 + r, n) / (Math.pow(1 + r, n) - 1);
+      return M.toFixed(2); // round to 2 decimal places
+    } else if (type === "interest-only") {
+      const interestOnlyMonthly = P * r; // Use monthly interest rate
+      return `${interestOnlyMonthly.toFixed(2)}`; // round to 2 decimal places
+    } else {
+      return "Invalid mortgage type.";
+    }
+  };
 
   const formatNumber = (num) =>{
     return new Intl.NumberFormat('en-US').format(num.replace(/\D/g, ""));
@@ -67,8 +93,21 @@ export default function Board() {
       interestRate: interestRate === "",
       type: selectedType === ""
     };
-    console.log(newErrors)
     setErrors(newErrors);
+    const mothlyResult = calculateMortgage(mortgageAmount, mortgageTerm, interestRate, selectedType);
+    setMonthly(mothlyResult);
+    setTotal(formatNumber(String(mothlyResult * mortgageTerm * 12)));
+    controls.start({
+      y: -100, // Example animation: fade in
+      opacity: 0,
+      transition: { duration: 0.25 }, // 0.5s transition
+    });
+    result.start({
+      y: 0, // Example animation: fade in
+      opacity: 100,
+      transition: { duration: 0.25 }, // 0.5s transition
+      visibility:"visible"
+    });
   };
 
   const handleCurrencyChange = (event) => {
@@ -96,33 +135,57 @@ export default function Board() {
         <div className="calculatorContainer">
           <div className="userInputContainer">
             <div className="mortageTitleContainer">
-              <h1 className="titleText">Mortage Calculator</h1>
+              <h1 className="titleText">Mortgage Calculator</h1>
               <button onClick={clearAll} className="clearBtn">Clear All</button>
             </div>
-            <UserInputField errors={errors.mortgageAmount} change={handleCurrencyChange} value={mortgageAmount} label="Mortage Amount" format="£" formatLeft={true}/>
+            <UserInputField errors={errors.mortgageAmount} change={handleCurrencyChange} value={mortgageAmount} label="Mortgage Amount" format="£" formatLeft={true}/>
             <div className="termRateContainer">
               <UserInputField errors={errors.mortgageTerm} change={handleMortage} value={mortgageTerm} label="Mortgage Term" format="years" formatLeft={false}/>
               <UserInputField errors={errors.interestRate} change={handleRate} value={interestRate} label="Interest Rate" format="%" formatLeft={false}/>
             </div>
             <div className="radioContainer">
               <h1 className="inputLabel">Mortgage Type</h1>
-              <label className={`selectContainer ${selectedType === "Repayment" ? "selected" : ""}`} onClick={() => handleLabelClick('Repayment')}>
-                <input className="radio" type="radio" name="mortgage" value="selectedType" checked={selectedType === 'Repayment'}/>
+              <label className={`selectContainer ${selectedType === "repayment" ? "selected" : ""}`} onClick={() => handleLabelClick('repayment')}>
+                <input className="radio" type="radio" name="mortgage" value="repayment" checked={selectedType === 'repayment'}/>
                 <h1>Repayment</h1>
               </label>
-              <label className={`selectContainer ${selectedType === "Interest" ? "selected" : ""}`}>
-                <input checked={selectedType === 'Interest'} className="radio" type="radio" name="mortgage" value="Interest" onClick={() => handleLabelClick('Interest')}/>
+              <label className={`selectContainer ${selectedType === "interest-only" ? "selected" : ""}`}>
+                <input checked={selectedType === 'interest-only'} className="radio" type="radio" name="mortgage" value="interest-only" onClick={() => handleLabelClick('interest-only')}/>
                 <h1>Interest Only</h1>
               </label>
               {errors.type && (<p className="errorText">This field is required</p>)}
             </div>
-            <button onClick={handleSubmit} className="submitButton">
-              <img src={icon} ></img>
-              <h1>Calculate Repayments</h1>
-            </button>
+            <div className="inputContainer">
+              <button onClick={handleSubmit} className="submitButton">
+                <img alt="icon" src={icon} ></img>
+                <h1>Calculate Repayments</h1>
+              </button>
+            </div>
           </div>
           <div className="outputContainer">
-
+            <motion.div animate={controls} initial={{ y:0, opacity:100}} className="outputPreview">
+              <img alt="illustration" src={illustration}></img>
+              <h1>Results shown here</h1>
+              <p>Complete the form and click "calculate repayments" to see what your monthly repayments would be.</p>
+            </motion.div>
+            <motion.div animate={result} initial={{ y:100, opacity:0, visibility: "hidden"}} className="resultPreview">
+              <h1>Your results</h1>
+              <p>Your results are shown below based on the information you provided. To adjust 
+                the results, edit the form and click "calculate repayments" again.
+              </p>
+              <div className="resultContainer">
+                <div className="result">
+                  <div className="monthlyContainer">
+                    <p>Your monthly repayments</p>
+                    <h1>£{monthly}</h1>
+                  </div>
+                  <div className="totalContainer">
+                    <p>Total you'll repay over the term</p>
+                    <h1>£{total}</h1>
+                  </div>
+                </div>
+              </div>
+            </motion.div>
           </div>
         </div>
     </div>
